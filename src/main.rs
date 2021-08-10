a23e5a42b560ff54b8ec0f8c63cb54cf95e5e0b4
Stephan Creutz <stephan.cr@gmx.de>
2021-08-10 20:30:54 +0200

    fetch Arch Linux RSS feed and show newly uploaded packages

---
+++ src/main.rs
@@ -0,0 +1,109 @@
+#![warn(rust_2018_idioms)]
+
+use colored::Colorize;
+use http_req::request;
+use regex::RegexSet;
+use rss::Channel;
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, ErrorKind, Read};
+use toml::Value;
+use xdg::BaseDirectories;
+
+fn parse_filter_regexes(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let value = input.parse::<Value>()?;
+    let mut result = vec![];
+
+    match value {
+        toml::Value::Table(ref table) => match table.get("filter_set") {
+            Some(Value::Array(ref array)) => {
+                for elem in array {
+                    match elem {
+                        Value::String(string) => result.push(string.clone()),
+                        _ => return Err(String::from("unexpected type").into()),
+                    }
+                }
+            }
+            _ => return Err(String::from("unexpected type").into()),
+        },
+        _ => return Err(String::from("unexpected type").into()),
+    };
+
+    Ok(result)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let xdg_dirs = BaseDirectories::new()?;
+    let config_path = xdg_dirs
+        .find_config_file("arch-package-list-fetcher.config")
+        .ok_or_else(|| io::Error::new(ErrorKind::NotFound, "not found"))?;
+    let mut input = String::new();
+    File::open(&config_path).and_then(|mut f| f.read_to_string(&mut input))?;
+
+    let filter_regexes = parse_filter_regexes(&input)?;
+    let set = RegexSet::new(&filter_regexes).unwrap();
+
+    let mut content = Vec::new();
+    let res = request::get("https://archlinux.org/feeds/packages/", &mut content)?;
+
+    if res.status_code().is_success() {
+        let channel = Channel::read_from(&content[..])?;
+        for item in channel.into_items() {
+            let title = if let Some(title) = item.title() {
+                title
+            } else {
+                "unknown title"
+            };
+            if set.is_match(title) {
+                continue;
+            }
+
+            if item
+                .categories()
+                .iter()
+                .any(|cat| cat.name().contains("Testing"))
+            {
+                continue;
+            }
+
+            let v: Vec<&str> = title.split(' ').collect();
+            let package_name = &v[0];
+            let package_version = &v[1];
+            println!("{} {}", package_name.green(), package_version.red());
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+    use toml::Value;
+
+    #[test]
+    fn test_config() -> Result<(), io::Error> {
+        let toml_str = r#"
+            filter_set = [ "^haskell-", "^php\\d?-?" ]
+        "#;
+
+        let value = toml_str.parse::<Value>()?;
+        match value {
+            Value::Table(ref table) => match table.get("filter_set") {
+                Some(Value::Array(ref array)) => {
+                    for elem in array {
+                        match elem {
+                            Value::String(string) => eprintln!("string {}", string),
+                            _ => panic!("unexpected type"),
+                        }
+                    }
+                }
+                _ => panic!("unexpected type"),
+            },
+            _ => panic!("unexpected type"),
+        }
+        eprintln!("{:?}", value);
+
+        Ok(())
+    }
+}
